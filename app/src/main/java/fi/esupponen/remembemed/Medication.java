f144e3f32af9854ecd4d7b1661fe8b3972f7eb11
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-03-21 19:37:15 +0200

    Medication: getters and setters

--- app/src/main/java/fi/esupponen/remembemed/Medication.java
+++ app/src/main/java/fi/esupponen/remembemed/Medication.java
@@ -8,4 +8,20 @@ public class Medication {
         this.name = name;
         this.dose = dose;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDose() {
+        return dose;
+    }
+
+    public void setDose(String dose) {
+        this.dose = dose;
+    }
 }
