33508353736ccebc2f2ea7dd5557020406a46465
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-03-21 19:36:32 +0200

    Medication: constructor

--- app/src/main/java/fi/esupponen/remembemed/Medication.java
+++ app/src/main/java/fi/esupponen/remembemed/Medication.java
@@ -3,4 +3,9 @@ package fi.esupponen.remembemed;
 public class Medication {
     String name;
     String dose;
+
+    public Medication(String name, String dose) {
+        this.name = name;
+        this.dose = dose;
+    }
 }
