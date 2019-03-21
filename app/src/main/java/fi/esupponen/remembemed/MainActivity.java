fcb88cd3fbf3376f229e174796ab6732a09e61ff
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-03-21 20:34:27 +0200

    MainActivity, setExampleAlarm: finds the current hour and minute (because Calender doesnt work before SdkVersion 24)

--- app/src/main/java/fi/esupponen/remembemed/MainActivity.java
+++ app/src/main/java/fi/esupponen/remembemed/MainActivity.java
@@ -1,15 +1,32 @@
 package fi.esupponen.remembemed;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import java.sql.Time;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<Medication> medications;
+    private ArrayList<Medication> medications;
+    private AlarmManager alarmManager;
+    private PendingIntent alarmIntent;
 
     public void setExampleAlarm(int hour, int minute) {
+        alarmManager = (AlarmManager)getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(getApplicationContext(), AlarmReceiver.class);
+        alarmIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, 0);
 
+        Time now = new Time(System.currentTimeMillis());
+
+        int currentHour = now.getHours();
+        int currentMin = now.getMinutes();
+
+        Log.d("MainActivity", "hour: " + currentHour + " min: " + currentMin);
     }
 
     @Override
@@ -18,5 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setExampleAlarm(0,0);
     }
 }
