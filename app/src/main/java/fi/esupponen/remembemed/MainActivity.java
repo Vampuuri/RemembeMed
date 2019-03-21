c5b2e75a8e1d6d9c9d33462e5b1910b18f56bbf2
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-03-21 19:50:44 +0200

    MainActivity: set arraylist for medications

--- app/src/main/java/fi/esupponen/remembemed/MainActivity.java
+++ app/src/main/java/fi/esupponen/remembemed/MainActivity.java
@@ -3,10 +3,15 @@ package fi.esupponen.remembemed;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    ArrayList<Medication> medications;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        medications = new ArrayList<>();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
