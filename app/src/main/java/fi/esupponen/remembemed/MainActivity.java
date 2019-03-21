2eb0b0d29a09d248cf4330d1ec0738456d7642dd
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-03-21 19:52:35 +0200

    MainActivity: add setExampleAlarm() for testing purposes

--- app/src/main/java/fi/esupponen/remembemed/MainActivity.java
+++ app/src/main/java/fi/esupponen/remembemed/MainActivity.java
@@ -8,6 +8,10 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     ArrayList<Medication> medications;
 
+    public void setExampleAlarm(int hour, int minute) {
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         medications = new ArrayList<>();
