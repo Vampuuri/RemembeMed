3fb8463880459d3accafa5c33c71c0c6c65a9b8d
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-03-21 20:03:10 +0200

    AlarmReceiver: add class and onReceive
    Makes a debug log, when receives broadcast.

---
+++ app/src/main/java/fi/esupponen/remembemed/AlarmReceiver.java
@@ -0,0 +1,14 @@
+package fi.esupponen.remembemed;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("AlarmReceiver", "Alarm fired");
+    }
+}
