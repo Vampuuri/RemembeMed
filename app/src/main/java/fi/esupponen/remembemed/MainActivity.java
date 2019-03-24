51e3b8e78c2f382c0d75e5c71690d86c5d55e279
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-03-25 00:53:41 +0200

    MainActivity: remove info.json, because I'm dumb

--- app/src/main/java/fi/esupponen/remembemed/MainActivity.java
+++ app/src/main/java/fi/esupponen/remembemed/MainActivity.java
@@ -152,7 +152,7 @@ public class MainActivity extends AppCompatActivity {
         Writer writer = new StringWriter();
 
         try {
-            InputStream infoInput = this.getAssets().open("info.json");
+            InputStream infoInput = openFileInput("info.json");
             BufferedReader reader = new BufferedReader(new InputStreamReader(infoInput, "UTF-8"));
             String line = reader.readLine();
 
@@ -171,7 +171,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void writeJsonFile() {
+        JSONObject jsonObject = makeJsonObject();
 
+        try {
+            Writer output = null;
+            File file = new File(this.getFilesDir(), "info.json");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public JSONObject makeJsonObject() {
