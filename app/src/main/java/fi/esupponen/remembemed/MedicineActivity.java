aab11d604d528a3f93e92f3f8cf54a832ebe7202
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-02 21:13:00 +0300

    MedicineActivity: puts medicine name in layout

--- app/src/main/java/fi/esupponen/remembemed/MedicineActivity.java
+++ app/src/main/java/fi/esupponen/remembemed/MedicineActivity.java
@@ -2,12 +2,20 @@ package fi.esupponen.remembemed;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MedicineActivity extends AppCompatActivity {
+    Medication medication;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medicine);
+
+        Bundle b = getIntent().getExtras();
+        medication = (Medication) b.getSerializable("medication");
+
+        TextView tv = (TextView) findViewById(R.id.textplace);
+        tv.setText(medication.getName());
     }
 }
