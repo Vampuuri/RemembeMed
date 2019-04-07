5fd9283eea3759badb394be04badd48928036680
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-08 00:10:11 +0300

    AlarmReceiver: Actually fire alarm when sdk version is over 25

--- app/src/main/java/fi/esupponen/remembemed/AlarmReceiver.java
+++ app/src/main/java/fi/esupponen/remembemed/AlarmReceiver.java
@@ -1,14 +1,48 @@
 package fi.esupponen.remembemed;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver {
+    String CHANNEL_ID = "medicationChannel";
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d("AlarmReceiver", "Alarm fired");
+        NotificationCompat.Builder builder;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Medicine alarms";
+            String description = "Alarms for medicines";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+            builder = new NotificationCompat.Builder(context, CHANNEL_ID);
+        } else {
+            builder = new NotificationCompat.Builder(context);
+        }
+
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("Time to take medication!");
+        builder.setContentText("Medication info here");
+
+        Notification notification = builder.build();
+        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        int id = 0;
+        manager.notify(id, notification);
     }
 }
