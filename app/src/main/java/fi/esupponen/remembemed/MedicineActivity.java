5407d78cb4414a421e7cd3ab7317732993151ecb
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-07 11:36:38 +0300

    MedicineActivity: Add methods for buttons

--- app/src/main/java/fi/esupponen/remembemed/MedicineActivity.java
+++ app/src/main/java/fi/esupponen/remembemed/MedicineActivity.java
@@ -2,10 +2,28 @@ package fi.esupponen.remembemed;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class MedicineActivity extends AppCompatActivity {
     Medication medication;
+
+    public void editMedicineName(View v) {
+        Log.d("MedicineActivity", "Edit name");
+    }
+
+    public void editMedicineDose(View v) {
+        Log.d("MedicineActivity", "Edit dose");
+    }
+
+    public void addNewAlarm(View v) {
+        Log.d("MedicineActivity", "Add alarm");
+    }
+
+    public void deleteMedication(View v) {
+        Log.d("MedicineActivity", "Delete medication");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
