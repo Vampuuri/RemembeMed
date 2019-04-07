bbf2800eb6c84be683ee5b259f8941f04667a51b
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-08 01:48:34 +0300

    MedicineActivity: send alarm data to MainActivity

--- app/src/main/java/fi/esupponen/remembemed/MedicineActivity.java
+++ app/src/main/java/fi/esupponen/remembemed/MedicineActivity.java
@@ -13,7 +13,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class MedicineActivity extends AppCompatActivity implements EditDialogFragment.EditDialogListener, AddAlarmDialogFragment.AddAlarmDialogFragmentListener {
     Medication medication;
@@ -44,6 +47,19 @@ public class MedicineActivity extends AppCompatActivity implements EditDialogFra
         super.finish();
     }
 
+    public void showAlarms() {
+        List<Alarm> alarms = medication.getAlarms();
+        Alarm[] alarmArray = new Alarm[alarms.size()];
+
+        for (int i = 0; i < alarms.size(); i++) {
+            alarmArray[i] = alarms.get(i);
+        }
+
+        ListView list = (ListView) findViewById(R.id.alarmsList);
+        AlarmArrayAdapter adapter = new AlarmArrayAdapter(this, alarmArray);
+        list.setAdapter(adapter);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,6 +74,10 @@ public class MedicineActivity extends AppCompatActivity implements EditDialogFra
 
         TextView tvDosage = (TextView) findViewById(R.id.dosageView);
         tvDosage.setText(medication.getDose());
+
+        if (medication.getAlarms().size() > 0) {
+            showAlarms();
+        }
     }
 
     @Override
@@ -103,5 +123,13 @@ public class MedicineActivity extends AppCompatActivity implements EditDialogFra
 
         Alarm alarm = new Alarm(1, hours, minutes, (float)repeatAfterHour, true);
         medication.getAlarms().add(alarm);
+
+        showAlarms();
+
+        Intent broadcastIntent = new Intent("modify-data");
+        broadcastIntent.putExtra("request", MedicationRequest.ADD_ALARM);
+        broadcastIntent.putExtra("index", index);
+        broadcastIntent.putExtra("alarm", alarm);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(broadcastIntent);
     }
 }
