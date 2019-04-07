2a94d23c793e2a910484202a027198b9bf149aa2
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-07 12:37:07 +0300

    EditDialogFragment: initial code for testing

--- app/src/main/java/fi/esupponen/remembemed/EditDialogFragment.java
+++ app/src/main/java/fi/esupponen/remembemed/EditDialogFragment.java
@@ -1,6 +1,33 @@
 package fi.esupponen.remembemed;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
 
 public class EditDialogFragment extends DialogFragment {
+    public static EditDialogFragment getInstance() {
+        EditDialogFragment frag = new EditDialogFragment();
+
+        return frag;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstance) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("works").setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            }).setNegativeButton("not ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+
+        return builder.create();
+    }
 }
