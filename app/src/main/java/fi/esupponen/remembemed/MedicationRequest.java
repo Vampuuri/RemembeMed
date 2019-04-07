1f6ef9b510930fa6f16c6ebe7c0f66c34d8fba53
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-08 02:36:52 +0300

    MedicationRequest: new enum SET_TAKEN

--- app/src/main/java/fi/esupponen/remembemed/MedicationRequest.java
+++ app/src/main/java/fi/esupponen/remembemed/MedicationRequest.java
@@ -3,5 +3,5 @@ package fi.esupponen.remembemed;
 import java.io.Serializable;
 
 public enum MedicationRequest implements Serializable {
-    CHANGE_NAME, CHANGE_DOSE, DELETE, UPDATE, ADD_ALARM
+    CHANGE_NAME, CHANGE_DOSE, DELETE, UPDATE, ADD_ALARM, SET_TAKEN
 }
