91624c130ed949ced48a5a6478e99d2c43898dee
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-07 17:41:42 +0300

    TimeDialogFragment: implement timepicker

--- app/src/main/java/fi/esupponen/remembemed/TimeDialogFragment.java
+++ app/src/main/java/fi/esupponen/remembemed/TimeDialogFragment.java
@@ -1,6 +1,26 @@
 package fi.esupponen.remembemed;
 
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.icu.util.Calendar;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.widget.TimePicker;
 
-public class TimeDialogFragment extends DialogFragment {
+public class TimeDialogFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(getActivity(), this, hour, minute, true);
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Log.d("onTimeSet", hourOfDay + ":" + minute);
+    }
 }
