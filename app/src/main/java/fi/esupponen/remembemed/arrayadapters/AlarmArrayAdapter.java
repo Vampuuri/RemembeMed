18e7267f05286b1cbee24acc5e7060ba5fb2afc8
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-22 18:39:14 +0300

    AlarmArrayAdapter: rename CancelAlarm to AlarmManaging, add more methods

--- app/src/main/java/fi/esupponen/remembemed/arrayadapters/AlarmArrayAdapter.java
+++ app/src/main/java/fi/esupponen/remembemed/arrayadapters/AlarmArrayAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
@@ -13,19 +14,23 @@ import fi.esupponen.remembemed.R;
 
 public class AlarmArrayAdapter extends ArrayAdapter {
     private final Activity context;
+    private AlarmManaging manager;
     private Alarm[] alarms;
 
-    interface CancelAlarmListener {
+    public interface AlarmManaging {
         void cancelAlarm(int position);
+        void activateAlarm(int position);
+        void removeAlarm(int position);
     }
 
-    public AlarmArrayAdapter(Activity context, Alarm[] alarms) {
+    public AlarmArrayAdapter(Activity context, AlarmManaging manager, Alarm[] alarms) {
         super(context, R.layout.list_item, new String[alarms.length]);
         this.context = context;
+        this.manager = manager;
         this.alarms = alarms;
     }
 
-    public View getView(int position, View view, ViewGroup parent) {
+    public View getView(final int position, View view, ViewGroup parent) {
         LayoutInflater layoutInflater = context.getLayoutInflater();
         View listItemView = layoutInflater.inflate(R.layout.list_alarmitem, null, true);
 
@@ -45,6 +50,13 @@ public class AlarmArrayAdapter extends ArrayAdapter {
         }
 
         doseTextView.setText(alarms[position].getDose());
+
+        ((Button)listItemView.findViewById(R.id.buttonDeleteAlarm)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                manager.cancelAlarm(position);
+            }
+        });
 
         return listItemView;
     }
