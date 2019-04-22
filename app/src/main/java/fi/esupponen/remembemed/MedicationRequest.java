a3b121442a3e098a82671b1f29db71b5063f8289
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-23 00:28:38 +0300

    MedicationRequest: add javadoc

--- app/src/main/java/fi/esupponen/remembemed/MedicationRequest.java
+++ app/src/main/java/fi/esupponen/remembemed/MedicationRequest.java
@@ -2,6 +2,11 @@ package fi.esupponen.remembemed;
 
 import java.io.Serializable;
 
+/**
+ * @author Essi Supponen [essi.supponen@tuni.fi]
+ * @version 2019-04-23
+ * @since 1.8
+ */
 public enum MedicationRequest implements Serializable {
     CHANGE_NAME, DELETE, UPDATE, ADD_ALARM, REMOVE_ALARM, SET_ALARM_ACTIVE, SET_TAKEN
 }
