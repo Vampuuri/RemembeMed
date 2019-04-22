5ae3d8c9032ec06713bf7d34d8bc33d7637ee3a4
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-23 00:27:21 +0300

    AlarmReceiver: add javadoc

--- app/src/main/java/fi/esupponen/remembemed/AlarmReceiver.java
+++ app/src/main/java/fi/esupponen/remembemed/AlarmReceiver.java
@@ -9,18 +9,30 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
-import android.widget.Toast;
 
+/**
+ * @author Essi Supponen [essi.supponen@tuni.fi]
+ * @version 2019-04-23
+ * @since 1.8
+ */
 public class AlarmReceiver extends BroadcastReceiver {
+    /**
+     * Id for notification channel.
+     */
     String CHANNEL_ID = "medicationChannel";
 
+    /**
+     * Create alarm notification from information from the intent.
+     *
+     * @param context
+     * @param intent
+     */
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("AlarmReceiver", "Alarm fired");
         NotificationCompat.Builder builder;
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Newer versions need channel for all notifications.
             CharSequence name = "Medicine alarms";
             String description = "Alarms for medicines";
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
@@ -31,22 +43,26 @@ public class AlarmReceiver extends BroadcastReceiver {
 
             builder = new NotificationCompat.Builder(context, CHANNEL_ID);
         } else {
+            // Older version do not.
             builder = new NotificationCompat.Builder(context);
         }
 
+        // Create intent for opening the app
         Intent openIntent = new Intent(context, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, openIntent, 0);
 
+        // Set content of notification
         builder.setSmallIcon(R.mipmap.ic_launcher);
         builder.setContentTitle("Time to take medication!");
-        //builder.setContentText("Medication information here when i get the intent to work");
         builder.setContentText(intent.getExtras().getCharSequence("medName") + ": " + intent.getExtras().getCharSequence("medDose"));
         builder.setContentIntent(pendingIntent);
 
+        // Build notification
         Notification notification = builder.build();
         notification.flags |= Notification.FLAG_AUTO_CANCEL;
         NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
+        // Show notification
         int id = 0;
         manager.notify(id, notification);
     }
