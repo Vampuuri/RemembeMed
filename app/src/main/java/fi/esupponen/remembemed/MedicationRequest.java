9c5fa756ac77903e89dba5aacf15f9879e46caa1
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-23 00:27:51 +0300

    MedicationRequest: add SET_ALARM_ACTIVE

--- app/src/main/java/fi/esupponen/remembemed/MedicationRequest.java
+++ app/src/main/java/fi/esupponen/remembemed/MedicationRequest.java
@@ -3,5 +3,5 @@ package fi.esupponen.remembemed;
 import java.io.Serializable;
 
 public enum MedicationRequest implements Serializable {
-    CHANGE_NAME, DELETE, UPDATE, ADD_ALARM, REMOVE_ALARM, SET_TAKEN
+    CHANGE_NAME, DELETE, UPDATE, ADD_ALARM, REMOVE_ALARM, SET_ALARM_ACTIVE, SET_TAKEN
 }
