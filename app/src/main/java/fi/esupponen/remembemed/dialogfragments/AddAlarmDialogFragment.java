22cab42150bc3348790b4782bb6a7b00b44643a2
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-23 00:14:09 +0300

    AddAlarmDialogFragment: add javadoc

--- app/src/main/java/fi/esupponen/remembemed/dialogfragments/AddAlarmDialogFragment.java
+++ app/src/main/java/fi/esupponen/remembemed/dialogfragments/AddAlarmDialogFragment.java
@@ -8,7 +8,6 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -18,25 +17,74 @@ import android.widget.TextView;
 
 import fi.esupponen.remembemed.R;
 
+/**
+ * @author Essi Supponen [essi.supponen@tuni.fi]
+ * @version 2019-04-23
+ * @since 1.8
+ */
 public class AddAlarmDialogFragment extends DialogFragment implements View.OnClickListener {
+
+    /**
+     * Layout of the dialog.
+     */
     View layout;
+
+    /**
+     * Hour of day of the alarm.
+     */
     int hours;
+
+    /**
+     * Minute of day of the alarm.
+     */
     int minutes;
+
+    /**
+     * Hours between repeats, 0 = no repeats.
+     */
     double repeatAfterHour;
+
+    /**
+     * Dose of the alarm.
+     */
     String dose;
 
+    /**
+     * Listener for adding alarm.
+     */
     AddAlarmDialogFragmentListener listener;
 
+    /**
+     * Interface for adding alarms.
+     */
     public interface AddAlarmDialogFragmentListener {
+
+        /**
+         * Adds alarm.
+         *
+         * @param hours
+         * @param minutes
+         * @param repeatAfterHour
+         * @param dose
+         */
         void addAlarm(int hours, int minutes, double repeatAfterHour, String dose);
     }
 
+    /**
+     * Opens dialog for setting time.
+     */
     public void openSelectTimeFragment() {
         TimeDialogFragment frag = new TimeDialogFragment();
         frag.setParent(this);
         frag.show(getFragmentManager(), "timeDialogFragment");
     }
 
+    /**
+     * Sets hour of day and minute of day of the alarm.
+     *
+     * @param hours     hour of day
+     * @param minutes   minute of day
+     */
     public void setTime(int hours, int minutes) {
         this.hours = hours;
         this.minutes = minutes;
@@ -45,24 +93,36 @@ public class AddAlarmDialogFragment extends DialogFragment implements View.OnCli
         ));
     }
 
+    /**
+     * Creates dialog and adds listeners to buttons.
+     *
+     * @param savedInstance
+     * @return dialog
+     */
     @Override
     public Dialog onCreateDialog(Bundle savedInstance) {
+
+        // Set initial values
         hours = 0;
         minutes = 0;
         repeatAfterHour = 0;
         dose = "";
 
+        // Set builder
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
+        // Inflate layout
         LayoutInflater inflater = getActivity().getLayoutInflater();
         layout = inflater.inflate(R.layout.dialog_newalarm, null);
         ((RadioButton)layout.findViewById(R.id.radioButtonNoRepeat)).toggle();
 
+        // Set OnClickListener to radiobuttons
         ((RadioButton)layout.findViewById(R.id.radioButtonNoRepeat)).setOnClickListener(this);
         ((RadioButton)layout.findViewById(R.id.radioButton24h)).setOnClickListener(this);
         ((RadioButton)layout.findViewById(R.id.radioButton48h)).setOnClickListener(this);
         ((RadioButton)layout.findViewById(R.id.radioButton72h)).setOnClickListener(this);
 
+        // Set TextChangedListener for newAlarmDose
         ((EditText)layout.findViewById(R.id.newAlarmDose)).addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -80,8 +140,10 @@ public class AddAlarmDialogFragment extends DialogFragment implements View.OnCli
             }
         });
 
+        // set time to layout
         setTime(0,0);
 
+        // Add OnClickListener to addTimeButton
         Button addTimeButton = (Button) layout.findViewById(R.id.selectTimeButton);
         addTimeButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -90,8 +152,10 @@ public class AddAlarmDialogFragment extends DialogFragment implements View.OnCli
             }
         });
 
+        // Set layout to view
         builder.setView(layout);
 
+        // Set negative and positive button
         builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
@@ -101,17 +165,26 @@ public class AddAlarmDialogFragment extends DialogFragment implements View.OnCli
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Log.d("EditDialogFragment", "cancelled");
+                        // Do nothing
                     }
                 });
 
+        // Create and return dialog
         return builder.create();
     }
 
+    /**
+     * Sends gathered information to listener.
+     */
     public void sendAlarmInfo() {
         listener.addAlarm(hours,minutes,repeatAfterHour,dose);
     }
 
+    /**
+     * Checks if context is AddAlarmDialogFragmentListener.
+     *
+     * @param context
+     */
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -123,6 +196,11 @@ public class AddAlarmDialogFragment extends DialogFragment implements View.OnCli
         }
     }
 
+    /**
+     * Methdo for every radiobutton.
+     *
+     * @param view
+     */
     @Override
     public void onClick(View view) {
         boolean checked = ((RadioButton) view).isChecked();
@@ -149,8 +227,5 @@ public class AddAlarmDialogFragment extends DialogFragment implements View.OnCli
                 }
                 break;
         }
-
-
-        Log.d("AddAlarmDialog, onClick", "" + repeatAfterHour);
     }
 }
