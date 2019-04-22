40a6ad39154e1ad3283994805855345610ab4da9
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-23 00:18:28 +0300

    EditDialoFragment: add javadoc

--- app/src/main/java/fi/esupponen/remembemed/dialogfragments/EditDialogFragment.java
+++ app/src/main/java/fi/esupponen/remembemed/dialogfragments/EditDialogFragment.java
@@ -6,7 +6,6 @@ import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -16,17 +15,43 @@ import fi.esupponen.remembemed.classes.Medication;
 import fi.esupponen.remembemed.MedicationRequest;
 import fi.esupponen.remembemed.R;
 
+/**
+ * @author Essi Supponen [essi.supponen@tuni.fi]
+ * @version 2019-04-23
+ * @since 1.8
+ */
 public class EditDialogFragment extends DialogFragment {
 
+    /**
+     * Listener for EditDialog.
+     */
     EditDialogListener listener;
 
+    /**
+     * Interface for updating information.
+     */
     public interface EditDialogListener {
+        /**
+         * Sends updated info to listener.
+         *
+         * @param request
+         * @param updatedInfo
+         */
         void updateEditedDialog(MedicationRequest request, String updatedInfo);
     }
 
+    /**
+     * Initializes and returns EditDialog.
+     *
+     * @param medication
+     * @param request
+     * @return EditDialog
+     */
     public static EditDialogFragment getInstance(Medication medication, MedicationRequest request) {
         EditDialogFragment frag = new EditDialogFragment();
         Bundle args = new Bundle();
+
+        // Check which attribute to edit
         args.putSerializable("request", request);
         args.putSerializable("medication", medication);
 
@@ -39,16 +64,25 @@ public class EditDialogFragment extends DialogFragment {
         return frag;
     }
 
+    /**
+     * Creates and returns EditDialog
+     *
+     * @param savedInstance
+     * @return
+     */
     @Override
     public Dialog onCreateDialog(Bundle savedInstance) {
         Bundle args = getArguments();
 
+        // Get information from args
         Medication med = (Medication) args.getSerializable("medication");
         final MedicationRequest request = (MedicationRequest) args.getSerializable("request");
         final String field = args.getString("field");
 
+        // Get builder
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
+        // Inflate layout
         LayoutInflater inflater = getActivity().getLayoutInflater();
         final View layout = inflater.inflate(R.layout.dialog_edit, null);
         ((TextView)layout.findViewById(R.id.editFragmentTitle)).setText("Edit " + field);
@@ -58,6 +92,7 @@ public class EditDialogFragment extends DialogFragment {
 
         builder.setView(layout);
 
+        // Set OnClickListeners
         builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
@@ -67,13 +102,19 @@ public class EditDialogFragment extends DialogFragment {
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
-                    Log.d("EditDialogFragment", "cancelled");
+                    // do nothing
                 }
             });
 
+        // Build and return dialog
         return builder.create();
     }
 
+    /**
+     * Checks if context is EditDialogListener.
+     *
+     * @param context
+     */
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
