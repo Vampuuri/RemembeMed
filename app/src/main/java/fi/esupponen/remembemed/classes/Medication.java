57f8631377045b55a898be2e9be8bd7c06856cee
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-23 00:04:31 +0300

    Medication: add javadoc

--- app/src/main/java/fi/esupponen/remembemed/classes/Medication.java
+++ app/src/main/java/fi/esupponen/remembemed/classes/Medication.java
@@ -6,22 +6,50 @@ import java.util.LinkedList;
 import fi.esupponen.remembemed.classes.Alarm;
 
 public class Medication implements Serializable {
+
+    /**
+     * Name of the medication.
+     */
     String name;
+
+    /**
+     * List of all alarms medication has.
+     */
     LinkedList<Alarm> alarms;
 
+    /**
+     * Basic constructor.
+     *
+     * @param name
+     */
     public Medication(String name) {
         this.name = name;
         this.alarms = new LinkedList<>();
     }
 
+    /**
+     * Returns name.
+     *
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets given name.
+     *
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Return list of all alarms.
+     *
+     * @return alarms of medication.
+     */
     public LinkedList<Alarm> getAlarms() {
         return alarms;
     }
