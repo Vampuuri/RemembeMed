eb75409afb39e0afb82e8ae544a058dbda9cde11
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-22 23:19:16 +0300

    AlarmArrayAdapter: add javadoc

--- app/src/main/java/fi/esupponen/remembemed/arrayadapters/AlarmArrayAdapter.java
+++ app/src/main/java/fi/esupponen/remembemed/arrayadapters/AlarmArrayAdapter.java
@@ -13,18 +13,69 @@ import android.widget.TextView;
 import fi.esupponen.remembemed.classes.Alarm;
 import fi.esupponen.remembemed.R;
 
+/**
+ * @author Essi Supponen [essi.supponen@tuni.fi]
+ * @version 2019-04-19
+ * @since 1.8
+ */
 public class AlarmArrayAdapter extends ArrayAdapter {
+
+    /**
+     * Context of the array adapter, MedicationActivity
+     */
     private final Activity context;
+
+    /**
+     * AlarmManaging class, also MedicationActivity
+     */
     private AlarmManaging manager;
+
+    /**
+     * Array of alarms for ListView
+     */
     private Alarm[] alarms;
 
+    /**
+     * Interface for any methods needed for OnClickListeners and OnCheckedChangedListeners
+     */
     public interface AlarmManaging {
+        /**
+         * Cancel given alarm.
+         *
+         * @param position of alarm in the list
+         */
         void cancelAlarm(int position);
+
+        /**
+         * Activate given alarm.
+         *
+         * @param position of alarm in the list
+         */
         void activateAlarm(int position);
+
+        /**
+         * Remove given alarm.
+         *
+         * @param position of alarm in the list
+         */
         void removeAlarm(int position);
+
+        /**
+         * Set taken dose for alarm.
+         *
+         * @param position  of alarm in the list
+         * @param taken     is dose taken or not
+         */
         void setTakenAlarm(int position, boolean taken);
     }
 
+    /**
+     * Constructor for AlarmArrayAdapter
+     *
+     * @param context   context, MedicationActivity
+     * @param manager   AlarmManaging, also Mediation Activity
+     * @param alarms    array of alarms
+     */
     public AlarmArrayAdapter(Activity context, AlarmManaging manager, Alarm[] alarms) {
         super(context, R.layout.list_item, new String[alarms.length]);
         this.context = context;
@@ -32,14 +83,31 @@ public class AlarmArrayAdapter extends ArrayAdapter {
         this.alarms = alarms;
     }
 
+    /**
+     * Help method for taken CheckBox OnCheckedChangeListener
+     *
+     * @param onOff CheckBox to change
+     * @param isOn  new status
+     */
     private void setCheckedOnOff(CheckBox onOff, boolean isOn) {
         onOff.setChecked(isOn);
     }
 
+    /**
+     * Returns one view object of the list in the given position.
+     *
+     * @param position
+     * @param view
+     * @param parent
+     * @return one view object
+     */
     public View getView(final int position, View view, ViewGroup parent) {
+
+        // Create layout
         LayoutInflater layoutInflater = context.getLayoutInflater();
         View listItemView = layoutInflater.inflate(R.layout.list_alarmitem, null, true);
 
+        // Set checkBoxOnOff status and listener
         final CheckBox checkBoxOnOff = (CheckBox)listItemView.findViewById(R.id.buttonOnOff);
         checkBoxOnOff.setChecked(alarms[position].isAlarmOn());
         checkBoxOnOff.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -53,6 +121,7 @@ public class AlarmArrayAdapter extends ArrayAdapter {
             }
         });
 
+        // Set doseTakenCheckBox status and listener
         ((CheckBox)listItemView.findViewById(R.id.doseTakenCheckBox)).setChecked(alarms[position].isTaken());
         ((CheckBox)listItemView.findViewById(R.id.doseTakenCheckBox)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -64,10 +133,12 @@ public class AlarmArrayAdapter extends ArrayAdapter {
             }
         });
 
+        // Get TextViews from layout
         TextView titleTextView = listItemView.findViewById(R.id.alarmTitle);
         TextView repeatTextView = listItemView.findViewById(R.id.alarmInfo);
         TextView doseTextView = listItemView.findViewById(R.id.alarmDose);
 
+        // Set texts to TextViews
         titleTextView.setText(String.format(
                 "%02d:%02d", alarms[position].getHour(), alarms[position].getMinute()
         ));
@@ -79,6 +150,7 @@ public class AlarmArrayAdapter extends ArrayAdapter {
 
         doseTextView.setText(alarms[position].getDose());
 
+        // Set listener for removeButton
         ((Button)listItemView.findViewById(R.id.buttonDeleteAlarm)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
