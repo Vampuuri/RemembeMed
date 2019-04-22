48657add7a1b45f0154e9fd6249babbd164350de
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-23 00:21:29 +0300

    NewMedicationDialogFragment: add javadoc

--- app/src/main/java/fi/esupponen/remembemed/dialogfragments/NewMedicationDialogFragment.java
+++ app/src/main/java/fi/esupponen/remembemed/dialogfragments/NewMedicationDialogFragment.java
@@ -6,30 +6,52 @@ import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import fi.esupponen.remembemed.R;
 
+/**
+ * @author Essi Supponen [essi.supponen@tuni.fi]
+ * @version 2019-04-23
+ * @since 1.8
+ */
 public class NewMedicationDialogFragment extends DialogFragment {
 
+    /**
+     * Listener for NewMedicationFragment.
+     */
     NewMedicationFragmentListener listener;
 
+    /**
+     * Interface for adding new medication.
+     */
     public interface NewMedicationFragmentListener {
+        /**
+         * Adds new medication with given name.
+         *
+         * @param name
+         */
         void addNewMedication(String name);
     }
 
+    /**
+     * Creates and returns dialog.
+     *
+     * @param savedInstance
+     * @return NewMedicationDialog
+     */
     @Override
     public Dialog onCreateDialog(Bundle savedInstance) {
 
+        // Get builder
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
+        // Set layout
         final View layout = getActivity().getLayoutInflater().inflate(R.layout.dialog_newmed, null);
         builder.setView(layout);
 
+        // Set OnCLickListeners
         builder.setTitle("Add new medication")
                 .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
             @Override
@@ -41,13 +63,19 @@ public class NewMedicationDialogFragment extends DialogFragment {
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Log.d("EditDialogFragment", "cancelled");
+                        // do nothing
                     }
                 });
 
+        // Build and return dialog.
         return builder.create();
     }
 
+    /**
+     * Checks if context is MewMedicationFragmentListener.
+     *
+     * @param context
+     */
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
