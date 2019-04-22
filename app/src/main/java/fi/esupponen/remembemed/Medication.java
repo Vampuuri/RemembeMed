a12c3f4b544276acc0a1881d5df3cfe89f097879
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-22 11:27:14 +0300

    Medication: add getter for alarms

--- app/src/main/java/fi/esupponen/remembemed/Medication.java
+++ app/src/main/java/fi/esupponen/remembemed/Medication.java
@@ -22,4 +22,8 @@ public class Medication implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
+
+    public LinkedList<Alarm> getAlarms() {
+        return alarms;
+    }
 }
