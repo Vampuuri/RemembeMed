5641e8f54711f570141b02c5ba528444aaaa5feb
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-23 00:24:06 +0300

    TimeDialogFragment: add javadoc

--- app/src/main/java/fi/esupponen/remembemed/dialogfragments/TimeDialogFragment.java
+++ app/src/main/java/fi/esupponen/remembemed/dialogfragments/TimeDialogFragment.java
@@ -7,22 +7,50 @@ import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.widget.TimePicker;
 
+/**
+ * @author Essi Supponen [essi.supponen@tuni.fi]
+ * @version 2019-04-23
+ * @since 1.8
+ */
 public class TimeDialogFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+
+    /**
+     * AddAlarmDialogFragment that is TimeDialogFragment's parent.
+     */
     AddAlarmDialogFragment parent;
 
+    /**
+     * Sets parent.
+     *
+     * @param parent
+     */
     public void setParent(AddAlarmDialogFragment parent) {
         this.parent = parent;
     }
 
+    /**
+     * Creates and returns dialog.
+     *
+     * @param savedInstanceState
+     * @return TimeDialog
+     */
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final Calendar c = Calendar.getInstance();
         int hour = c.get(Calendar.HOUR_OF_DAY);
         int minute = c.get(Calendar.MINUTE);
 
+        // Create and return premade TimePickerDialog
         return new TimePickerDialog(getActivity(), this, hour, minute, true);
     }
 
+    /**
+     * Forwards selected time to parent.
+     *
+     * @param view
+     * @param hourOfDay
+     * @param minute
+     */
     @Override
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
         parent.setTime(hourOfDay, minute);
